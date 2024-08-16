547c51875cbc0551083e2766d3ec8a69db59b4e0
angelo <angelopazin@gmail.com>
2024-08-16 11:21:37 -0300

    More stuff done

--- aula_2/lists.hs
+++ aula_2/lists.hs
@@ -34,4 +34,68 @@ rangeList n = if n == 0
               then []
               else rangeList (n-1) ++ [n]
 
-main = print (rangeList 1)
+
+-- Reuturn how many times an elemnts appears in a list
+countElement _ [] = 0
+countElement x (a:as) = if x == a
+                 then 1 + countElement x as
+                 else countElement x as
+
+
+-- Returns the sum of the elements in a list
+soma [] = 0
+soma (a:as) = a + soma as
+
+
+-- Returns the sum of the even elements in a list
+somaPar [] = 0
+somaPar (a:as) = if (mod a 2) == 1
+                  then somaPar as
+                  else a + somaPar as
+
+
+-- Returns a list without its last element
+listNoLastElement (a:as) = if as == []
+                           then []
+                           else a:listNoLastElement as
+
+-- Return the sum of the elements in even positions
+-- List starts at index 1 
+sumEvenPos _ [] = 0
+sumEvenPos id (a:as) = if (mod id 2) == 1
+                    then sumEvenPos (id+1) as
+                    else a + sumEvenPos (id+1) as
+
+
+
+-- Returns the intercalation of 2 list
+-- When a list is finished, the function stops
+intercala1 as [] = []
+intercala1 [] bs = []
+intercala1 (a:as) (b:bs) = a:b:intercala1 as bs
+
+
+-- Returns the intercalation of 2 list
+-- When a list is finished, the function continues
+intercala2 as [] = as
+intercala2 [] bs = bs
+intercala2 (a:as) (b:bs) = a:b:intercala2 as bs
+
+
+-- Returns True if as list is ordered, False otherwise
+isListOrdered [] = True
+isListOrdered (a:as) = if as /= []
+                       then (a <= head as) && isListOrdered as
+                       else isListOrdered as
+
+-- Shift right in a list
+shiftr lista = let
+               lastItem = lastItemList lista
+               lista2 = listNoLastElement lista
+               in lastItem:lista2
+
+
+-- Shift right n times in a list
+shiftrn n lista = if list
+
+main = print (shiftr [2,4,6,8,10])

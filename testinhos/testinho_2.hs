ca20b78aefd8daa7cdbc03b490ac4e1b14ba7067
Turtle22 <angelopazin@gmail.com>
2024-09-02 16:55:13 -0300

    alternativa answer

--- testinhos/testinho_2.hs
+++ testinhos/testinho_2.hs
@@ -1,5 +1,11 @@
 chave_valor lista = (map fst lista, map snd lista)
 
 
+-- Implementação alternativa sem map
+separarElementos [] = ([], [])  -- Caso base: lista vazia
+separarElementos ((x, y):xs) = (x : xs', y : ys')
+  where
+    (xs', ys') = separarElementos xs
 
-main = print(chave_valor [("abc",1), ("de",4), ("casa",2)])
+
+main = print(separarElementos [("abc",1), ("de",4), ("casa",2)])
